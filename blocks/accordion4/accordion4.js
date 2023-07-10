3a316776065a4983d6e7c556f5881299e76f2bd9
unknown <sri749729@gmail.com>
2023-07-10 16:07:21 +0530

    [XMR-44] we havebwh.h

---
+++ blocks/accordion4/accordion4.js
@@ -0,0 +1,26 @@
+var a = document.querySelectorAll('.accordion4 div:nth-child(2)');
+a[0].style.display='none'
+var b = document.querySelectorAll('.accordion4 div:nth-child(1)>div')[0];
+var cr = document.createElement('span');
+cr.setAttribute('class','close')
+var cr1 = document.createElement('span');
+cr1.setAttribute('class','open')
+cr1.style.display="none"
+b.append(cr)
+b.append(cr1)
+var c = document.getElementsByClassName('close')[0];
+c.addEventListener("click",clse);
+function clse(){
+    document.querySelectorAll('.accordion4 div:nth-child(1)>div>span')[0].style.display="none"
+    document.getElementsByClassName('open')[0].style.display="block"
+    a[0].style.display='block';
+}
+var d = document.getElementsByClassName('open')[0];
+d.addEventListener("click",open);
+async function open(){
+    a[0].style.display='none'
+    document.querySelectorAll('.accordion4 div:nth-child(1)>div>span')[0].style.display="block"
+    document.getElementsByClassName('open')[0].style.display="none"
+
+}
+
