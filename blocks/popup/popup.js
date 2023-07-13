f08e6e3ef79df410f367be721110d2dd4b9fd94e
unknown <sri749729@gmail.com>
2023-07-13 07:18:11 +0530

    [XMR-44] we havebwh.

---
+++ blocks/popup/popup.js
@@ -0,0 +1,36 @@
+var xyz = document.querySelectorAll(".popup >div");
+for (var i = 0; i < xyz.length; i++) {
+  xyz[i].classList.add("container");
+}
+
+var ab = document.querySelectorAll(".popup .container div");
+
+for (var i = 0; i < ab.length; i++) {
+  if (i % 2 == 0) {
+    ab[i].classList.add("savings");
+  } else {
+    ab[i].classList.add("program");
+  }
+}
+
+
+const acc = document.getElementsByClassName("container");
+
+
+var panels = document.getElementsByClassName("savings");
+var i;
+var j;
+
+var handleAccordionClick = function () {
+  for (j = 0; j < panels.length; j++) {
+    if (acc[j] !== this) {
+      acc[j].classList.remove("active");
+    }
+  }
+  this.classList.toggle("active");
+};
+
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].onclick = handleAccordionClick;
+}
