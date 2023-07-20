93a3f4d5dccd3dd24d8bc79f2e9aabe8879cb219
unknown <sri749729@gmail.com>
2023-07-20 11:15:57 +0530

    [XMR-44] we havebwh.

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -4,12 +4,12 @@ pop1[0].classList.add("copay1");
 pop1[1].classList.add("copay2");
 
 pop1[0].addEventListener("click", function () {
-  pop1[0].classList.add("popd1");
-  pop1[1].classList.add("popd");
+  pop1[0].classList.add("cop1");
+  pop1[1].classList.add("cop");
 });
 
 var x = document.querySelector(".popup .copay2 h2");
 x.addEventListener("click", function () {
-  pop1[0].classList.remove("popd1");
-  pop1[1].classList.remove("popd");
+  pop1[0].classList.remove("cop1");
+  pop1[1].classList.remove("cop");
 });
